8e4dac66fad0bbed50a43d625af00c00ff01ef55
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-11 15:11:46 +0700

    Add project files.

---
+++ exes_2_140_c/exes_2_140_c.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int angga[36];
+int n;
+int i;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 36))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 36 elements.\n\n";
+    }
+    cout << "\n--------------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> angga[i];
+    }
+}
+
+void Display()
+{
+    char ch;
+    int ctr;
+
+    do
+    {
+        cout << "\nEnter the element you want to search: ";
+        int item;
+        cin >> item;
+
+        ctr = 0;
+        for (i = 0; i < n; i++)
+        {
+            ctr++;
+            if (angga[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl;
+                break;
+            }
+        }
+
+        if (i == n)
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nContinue search (y/n) ";
+        cin >> ch;
+    } while ((ch == 'y') || (ch == 'Y'));
+}
+
+int main()
+{
+    input();
+    Display();
+}
